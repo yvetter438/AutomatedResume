76084f6ef5546657f880f6ba9f7d5e31a67cbd11
yvetter438 <yannick.vetter@outlook.com>
2025-02-15 22:08:34 -0600

    Iteration 1.1 complete

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, send_file
-import json
+import os
+import subprocess
 
 app = Flask(__name__)
+
+
+def generate_pdf():
+    #define paths
+    template_path = os.path.join('templates', 'resume.tex')
+    out_dir = 'static'
+
+    #run pdflatex
+    subprocess.run([
+        'pdflatex',
+        '-output-directory', out_dir,
+        template_path
+    ])
+
+    return os.path.join(out_dir, 'resume.pdf')
 
 @app.route('/')
 def index():
@@ -10,8 +26,9 @@ def index():
 @app.route('/download-resume')
 def download_resume():
     # TODO: Implement PDF generation
+    pdf_path = generate_pdf()
     # For now, we'll just return a static PDF if you have one
-    return send_file('static/resume.pdf', as_attachment=True)
+    return send_file(pdf_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
